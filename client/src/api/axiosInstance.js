d429194bdcab0f8964d3eec8b1625e73b074ca65
Priyanshu <ribupkashyap@gmail.com>
2025-02-15 21:02:34 +0530

    added env file in client

--- client/src/api/axiosInstance.js
+++ client/src/api/axiosInstance.js
@@ -1,20 +1,20 @@
-import axios from 'axios';
+import axios from "axios";
 
 const axiosInstance = axios.create({
-    baseURL: 'http://localhost:5000',
+  baseURL: import.meta.env.VITE_API_URL,
 });
 
 axiosInstance.interceptors.request.use(
-    (config) => {
-      const accessToken = JSON.parse(sessionStorage.getItem("accessToken")) || "";
-  
-      if (accessToken) {
-        config.headers.Authorization = `Bearer ${accessToken}`;
-      }
-  
-      return config;
-    },
-    (err) => Promise.reject(err)
-  );
+  (config) => {
+    const accessToken = JSON.parse(sessionStorage.getItem("accessToken")) || "";
+
+    if (accessToken) {
+      config.headers.Authorization = `Bearer ${accessToken}`;
+    }
+
+    return config;
+  },
+  (err) => Promise.reject(err)
+);
 
 export default axiosInstance;
